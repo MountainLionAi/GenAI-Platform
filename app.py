0853fb01a71dbc5aa797ce040ccf1698402db9cd
rayhopf <rayhopf@protonmail.com>
2023-12-28 16:28:56 +0800

    update workers

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ if __name__ == "__main__":
         else:
             # workers的数量可以单独设置，如果设置为fast则默认为8
             # app.run(host=server.HOST, port=server.PORT, fast=True)
-            app.run(host=server.HOST, port=server.PORT)
+            app.run(host=server.HOST, port=server.PORT, workers=2)
