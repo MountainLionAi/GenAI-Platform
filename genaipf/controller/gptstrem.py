7711e7930dbda015724cfe114214fca9da23187d
rayhopf <rayhopf@protonmail.com>
2024-01-04 01:00:41 +0800

    refactor gptstream

--- genaipf/controller/gptstrem.py
+++ genaipf/controller/gptstrem.py
@@ -17,7 +17,7 @@ from datetime import datetime
 from genaipf.utils.log_utils import logger
 import time
 from pprint import pprint
-from genaipf.dispatcher.api import gpt_functions, afunc_gpt4_generator, aref_answer_gpt_generator
+from genaipf.dispatcher.api import generate_unique_id, get_format_output, gpt_functions, afunc_gpt_generator, aref_answer_gpt_generator
 from genaipf.dispatcher.utils import get_qa_vdb_topk, merge_ref_and_input_text
 from genaipf.dispatcher.prompts_v001 import LionPrompt
 # from dispatcher.gptfunction import unfiltered_gpt_functions, gpt_function_filter
@@ -123,13 +123,11 @@ async def  getAnswerAndCallGpt(question, userid, msggroup, language, front_messa
             messages.append({"role": x["role"], "content": x["content"]})
     user_history_l = [x["content"] for x in messages if x["role"] == "user"]
     newest_question = user_history_l[-1]
-    data = {}
     
     last_front_msg = front_messages[-1]
     question = last_front_msg['content']
     if last_front_msg.get("need_whisper"):
-        yield '[WHISPER]'
-        yield json.dumps({"text": last_front_msg['content']})
+        yield json.dumps(get_format_output("whisper", last_front_msg['content']))
     
     # vvvvvvvv 在第一次 func gpt 就准备好数据 vvvvvvvv
     related_qa = get_qa_vdb_topk(newest_question)
@@ -137,64 +135,29 @@ async def  getAnswerAndCallGpt(question, userid, msggroup, language, front_messa
     _messages = [x for x in messages if x["role"] != "system"]
     msgs = _messages[::]
     # ^^^^^^^^ 在第一次 func gpt 就准备好数据 ^^^^^^^^
-
     used_gpt_functions = gpt_function_filter(gpt_functions_mapping, _messages)
-    resp1 = await afunc_gpt4_generator(msgs, used_gpt_functions, language, model, "", related_qa)
+    _tmp_text = ""
+    data = {
+        'type' : 'gpt',
+        'content' : _tmp_text
+    }
+    resp1 = await afunc_gpt_generator(msgs, used_gpt_functions, language, model, "", related_qa)
     chunk = await resp1.__anext__()
-    _func_or_text = chunk.choices[0].delta.function_call
-    if _func_or_text:
-        mode1 = "func"
-        logger.info(f">>>>> activate gpt function >>>>>")
-    else:
-        mode1 = "text"
-    if mode1 == "text":
-        # c0 = chunk['choices'][0]['delta'].get("content", "")
-        c0 = chunk.choices[0].delta.content
-        _tmp_text = ""
-        _tmp_text += c0
-        yield '[GPT]'
-        _code = generate_unique_id()
-        yield json.dumps({"code": _code})
-        yield json.dumps({"text": c0})
+    assert chunk["role"] == "step"
+    if chunk["content"] == "llm_yielding":
         async for chunk in resp1:
-            # _gpt_letter = chunk['choices'][0]['delta'].get("content", "")
-            _gpt_letter = chunk.choices[0].delta.content
-            if _gpt_letter:
-                _tmp_text += _gpt_letter
-                yield json.dumps({"text": _gpt_letter})
-        if output_type == "voice":
-            # 对于语音输出，将文本转换为语音并编码
-            base64_encoded_voice = textToSpeech(_tmp_text)
-            yield '[TTS]'
-            yield json.dumps({
-                "role": "assistant", 
-                "type": "voice", 
-                "format": "mp3", 
-                "version": "v001", 
-                "content": base64_encoded_voice
-            })
-        yield "[DONE]"
-        data = {
-                'type' : 'gpt',
-                'content' : _tmp_text,
-                'code' : _code
-            }
-        logger.info(f'>>>>> text _tmp_text: {_tmp_text}')
-    elif mode1 == "func":
-        big_func_name = _func_or_text.name
-        func_name, sub_func_name = big_func_name.split("_____")
-        _arguments = _func_or_text.arguments
-        async for chunk in resp1:
-            # _func_json = chunk['choices'][0]['delta'].get("function_call", {})
-            _func_json = chunk.choices[0].delta.function_call
-            # print(f'>>>>> _func_json: {_func_json}')
-            if _func_json:
-                _arguments += _func_json.arguments
-            # _arguments += _func_json.get("arguments", "")
-        _param = json.loads(_arguments)
+            if chunk["role"] == "inner_____gpt_whole_text":
+                _tmp_text = chunk["content"]
+            else:
+                yield json.dumps(chunk)
+    elif chunk["content"] == "agent_routing":
+        chunk = await resp1.__anext__()
+        assert chunk["role"] == "inner_____func_param"
+        _param = chunk["content"]
         _param["language"] = language
-        _param["subtype"] = sub_func_name
-        logger.info(f'>>>>> func_name: {func_name}, sub_func_name: {sub_func_name}, _arguments: {_arguments}')
+        func_name = _param["func_name"]
+        sub_func_name = _param["subtype"]
+        logger.info(f'>>>>> func_name: {func_name}, sub_func_name: {sub_func_name}, _param: {_param}')
         t01 = time.time()
         logger.info(f'>>>>> gpt func time: {t01 - t0}')
         content = ""
@@ -207,68 +170,41 @@ async def  getAnswerAndCallGpt(question, userid, msggroup, language, front_messa
             _args = [_param.get(x) for x in preset_conf["param_names"]]
             presetContent, picked_content = await preset_conf["get_and_pick"](*_args)
             if preset_conf.get("has_preset_content") and (_param.get("need_chart") or preset_conf.get("need_preset")):
-                data = {
+                data.update({
                     'type' : _type,
                     'subtype': sub_func_name,
                     'content' : content,
                     'presetContent' : presetContent
-                }
-
+                })
         related_qa = get_qa_vdb_topk(newest_question)
         _messages = [x for x in messages if x["role"] != "system"]
         msgs = _messages[::]
         resp2 = await aref_answer_gpt_generator(msgs, model, language, _type, str(picked_content), related_qa)
-
-        # if data :
-        #     yield '[DATA]'
-        #     yield json.dumps(data)
-        _tmp_text = ""
         t1 = time.time()
         logger.info(f'>>>>> get data time: {t1 - t01}')
         logger.info(f'>>>>> start->data done t1 - t0: {t1 - t0}')
-        # if func_name in preset_entry_mapping:
-        #     yield '[GPTFUNC]'
-        #     _gptfunc_data = {"role": "gptfunc", "function_call": {"name": big_func_name, "arguments": _arguments}}
-        #     yield json.dumps(_gptfunc_data)
-        yield "[GPT]"
         async for chunk in resp2:
-            # _gpt_letter = chunk['choices'][0]['delta'].get("content", "")
-            _gpt_letter = chunk.choices[0].delta.content
-            # print(f'>>>>> _gpt_letter: {_gpt_letter}')
-            if _gpt_letter:
-                _tmp_text += _gpt_letter
-                yield json.dumps({"text": _gpt_letter})
+            if chunk["role"] == "inner_____gpt_whole_text":
+                _tmp_text = chunk["content"]
+            else:
+                yield json.dumps(chunk)
         posttexter = posttext_mapping.get(func_name)
         if posttexter is not None:
             async for _gpt_letter in posttexter.get_text_agenerator(PostTextParam(language, sub_func_name)):
                 _tmp_text += _gpt_letter
-                yield json.dumps({"text": _gpt_letter})
-        if output_type == "voice":
-            # 对于语音输出，将文本转换为语音并编码
-            base64_encoded_voice = textToSpeech(_tmp_text)
-            yield '[TTS]'
-            yield json.dumps({
-                "role": "assistant", 
-                "type": "voice", 
-                "format": "mp3", 
-                "version": "v001", 
-                "content": base64_encoded_voice
-            })
-        if len(data) == 0 :
-            data = {
-                'type' : 'gpt',
-                'content' : _tmp_text
-            }
-        else :
-            data['content'] = _tmp_text
-        # print(f'>>>>>test 002 : {data}')
-        if data:
-            _code = generate_unique_id()
-            data['code'] = _code
-            yield '[DATA]'
-            yield json.dumps(data)
-        yield "[DONE]"
-        logger.info(f'>>>>> func & ref _tmp_text & output_type: {output_type}: {_tmp_text}')
+                yield json.dumps(get_format_output("gpt", _gpt_letter))
+
+    if output_type == "voice":
+        # 对于语音输出，将文本转换为语音并编码
+        base64_encoded_voice = textToSpeech(_tmp_text)
+        yield json.dumps(get_format_output("tts", base64_encoded_voice, "voice_mp3_v001"))
+    _code = generate_unique_id()
+    data.update({
+        'content' : _tmp_text,
+        'code' : _code
+    })
+    yield json.dumps(get_format_output("step", "done"))
+    logger.info(f'>>>>> func & ref _tmp_text & output_type: {output_type}: {_tmp_text}')
 
     if question and msggroup :
         gpt_message = (
@@ -295,7 +231,3 @@ async def  getAnswerAndCallGpt(question, userid, msggroup, language, front_messa
 
     
 
-
-def generate_unique_id():
-    redis_client = RedisConnectionPool().get_connection()
-    return redis_client.incr('unique_id')
--- genaipf/dispatcher/api.py
+++ genaipf/dispatcher/api.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from genaipf.dispatcher.prompts_v001 import LionPrompt
 # from openai.error import InvalidRequestError
 from openai import BadRequestError
+from genaipf.utils.redis_utils import RedisConnectionPool
 
 # temperature=2 # 值在[0,1]之间，越大表示回复越具有不确定性
 # max_tokens=2000 # 输出的最大 token 数
@@ -17,7 +18,65 @@ top_p=0.85 # 过滤掉低于阈值的 token 确保结果不散漫
 frequency_penalty=0.3 # [-2,2]之间，该值越大则更倾向于产生不同的内容
 presence_penalty=0.2 # [-2,2]之间，该值越大则更倾向于产生不同的内容
 
-async def afunc_gpt4_generator(messages, functions=gpt_functions, language=LionPrompt.default_lang, model='', picked_content="", related_qa=[]):
+
+def generate_unique_id():
+    redis_client = RedisConnectionPool().get_connection()
+    return redis_client.incr('unique_id')
+
+def get_format_output(role, content, mode=None):
+    if mode == "voice_mp3_v001":
+        return {
+            "role": role, 
+            "type": "voice", 
+            "format": "mp3", 
+            "version": "v001", 
+            "content": content
+        }
+    else:
+        return {"role": role, "type": "text", "format": "text", "version": "v001", "content": content}
+
+async def aget_error_generator(msg="ERROR"):
+    for c in msg:
+        await asyncio.sleep(0.02)
+        yield get_format_output("error", c)
+
+async def awrap_gpt_generator(gpt_response):
+    resp = gpt_response
+    chunk = await resp.__anext__()
+    _func_or_text = chunk.choices[0].delta.function_call
+    if _func_or_text:
+        mode1 = "func"
+    else:
+        mode1 = "text"
+    if mode1 == "text":
+        yield get_format_output("step", "llm_yielding")
+        c0 = chunk.choices[0].delta.content
+        _tmp_text = ""
+        _tmp_text += c0
+        yield get_format_output("gpt", c0)
+        async for chunk in resp1:
+            _gpt_letter = chunk.choices[0].delta.content
+            if _gpt_letter:
+                _tmp_text += _gpt_letter
+                yield get_format_output("gpt", _gpt_letter)
+        yield get_format_output("inner_____gpt_whole_text", _tmp_text)
+    elif mode1 == "func":
+        yield get_format_output("step", "agent_routing")
+        big_func_name = _func_or_text.name
+        func_name, sub_func_name = big_func_name.split("_____")
+        _arguments = _func_or_text.arguments
+        async for chunk in resp1:
+            _func_json = chunk.choices[0].delta.function_call
+            if _func_json:
+                _arguments += _func_json.arguments
+        _param = json.loads(_arguments)
+        _param["func_name"] = func_name
+        _param["sub_func_name"] = sub_func_name
+        _param["subtype"] = sub_func_name
+        yield get_format_output("inner_____func_param", _param)
+        
+
+async def afunc_gpt_generator(messages, functions=gpt_functions, language=LionPrompt.default_lang, model='', picked_content="", related_qa=[]):
     '''
     "messages": [
         {"role": "user", "content": "Hello"},
@@ -49,16 +108,17 @@ async def afunc_gpt4_generator(messages, functions=gpt_functions, language=LionP
                 presence_penalty=presence_penalty,  # [-2,2]之间，该值越大则更倾向于产生不同的内容
                 stream=True
             )
-            print('afunc_gpt4_generator called')
-            return response
+            print('afunc_gpt_generator called')
+            return awrap_gpt_generator(response)
         except BadRequestError as e:
             print(e)
-            logger.error(f'afunc_gpt4_generator BadRequestError {e}', e)
+            logger.error(f'afunc_gpt_generator BadRequestError {e}', e)
             messages = messages[mlength // 2:]
         except Exception as e:
             print(e)
-            logger.error(f'afunc_gpt4_generator question_JSON call gpt4 error {e}', e)
-            raise e
+            logger.error(f'afunc_gpt_generator question_JSON call gpt4 error {e}', e)
+            return aget_error_generator(str(e))
+    return aget_error_generator("error after retry many times")
 
 
 async def aref_answer_gpt_generator(messages, model='', language=LionPrompt.default_lang, preset_name=None, picked_content="", related_qa=[]):
@@ -87,7 +147,7 @@ async def aref_answer_gpt_generator(messages, model='', language=LionPrompt.defa
                 stream=True
             )
             print(f'aref_answer_gpt called')
-            return response
+            return awrap_gpt_generator(response)
         except BadRequestError as e:
             print(e)
             logger.error(f'aref_answer_gpt_generator BadRequestError {e}', e)
@@ -95,7 +155,8 @@ async def aref_answer_gpt_generator(messages, model='', language=LionPrompt.defa
         except Exception as e:
             print(e)
             logger.error(f'aref_answer_gpt_generator question_JSON call gpt4 error {e}', e)
-
+            return aget_error_generator(str(e))
+    return aget_error_generator("error after retry many times")
 
 async def aref_oneshot_gpt_generator(messages, model='', language=LionPrompt.default_lang, preset_name=None, picked_content="", related_qa=[], data=None):
     front_messages = messages
