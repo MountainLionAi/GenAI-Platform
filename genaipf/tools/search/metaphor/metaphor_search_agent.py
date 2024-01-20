48bfb3d28d1e4009659c7ffab33977a78df55493
mikehahahaha <127082952+mikehahahaha@users.noreply.github.com>
2024-01-20 12:21:44 +0800

    1. metaphor 问题检索优化

--- genaipf/tools/search/metaphor/metaphor_search_agent.py
+++ genaipf/tools/search/metaphor/metaphor_search_agent.py
@@ -1,6 +1,6 @@
-from langchain.agents import AgentExecutor, OpenAIFunctionsAgent
+from langchain.agents import AgentExecutor, OpenAIFunctionsAgent, create_openai_functions_agent
 from langchain.schema import SystemMessage
-from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from genaipf.tools.search.metaphor.tools import tools
 
 
@@ -9,14 +9,17 @@ async def metaphor_search(question: str, language=None):
     question: 问题1
     language: en-英文;zh-中文
     """
-    llm = ChatOpenAI(temperature=0)
+    llm = ChatOpenAI(temperature=0, model='gpt-4')
+    question += " 将metaphor的search执行的5个结果参考{'search': [{'title': '', 'url': '', 'publishedDate': '', 'extract': '', 'author': ''}]}格式，find_similar执行的5个结果参考{'find_similar': [{'title': '', 'url': '', 'publishedDate': '', 'extract': '', 'author': ''}]}格式，get_contents执行的1个结果参考{'get_contents': {'title': '', 'url': '', 'publishedDate': '', 'extract': '', 'author': ''}}格式，并且将title，extract属性翻译成中文，返回例如: {'data': {'search': [],'find_similar': [],'get_contents': {}}}格式的json字符串。"
     content = "你是一个网络研究人员，通过在互联网上查找信息和检索有用文档的内容来回答用户的问题。引用资料来源。"
     if language == 'en':
         content = "You are a web researcher who answers user questions by looking up information on the internet and retrieving contents of helpful documents. Cite your sources."
+        question += " Return a JSON string in the format: {'data': {'search': [], 'find_similar': [], 'get_contents': {}}}, with references to the results of the 'search' operation in the format {'search': [{'title': '', 'url': '', 'publishedDate': '', 'extract': '', 'author': ''}]}, the 'find_similar' operation in the format {'find_similar': [{'title': '', 'url': '', 'publishedDate': '', 'extract': '', 'author': ''}]}, and the 'get_contents' operation in the format {'get_contents': {'title': '', 'url': '', 'publishedDate': '', 'extract': '', 'author': ''}}. For example: {'data': {'search': [], 'find_similar': [], 'get_contents': {}}}."
     system_message = SystemMessage(
         content=content
     )
     agent_prompt = OpenAIFunctionsAgent.create_prompt(system_message)
     agent = OpenAIFunctionsAgent(llm=llm, tools=tools, prompt=agent_prompt)
     agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-    return agent_executor.run(question)
+    result = agent_executor.run(question)
+    return result
