20ba22b941da6f3fd1666de6a93d98a08afa1766
mikehahahaha <127082952+mikehahahaha@users.noreply.github.com>
2024-01-23 14:28:37 +0800

    1. related search

--- genaipf/tools/search/utils/search_agent_utils.py
+++ genaipf/tools/search/utils/search_agent_utils.py
@@ -1,4 +1,5 @@
 from genaipf.tools.search.metaphor import metaphor_search_agent
+from genaipf.dispatcher.api import generate_unique_id, get_format_output, gpt_functions, afunc_gpt_generator
 
 
 # dict sources: [{'title': '', 'url': ''}]
@@ -11,3 +12,8 @@ async def other_search(question: str, related_qa=[], language=None):
 
     # -------- other --------
     return sources, related_qa
+
+
+async def related_search(question: str, model: str, language=None, related_qa=[]):
+    resp1 = await afunc_gpt_generator([question], gpt_functions, language, model, "", related_qa)
+    print(resp1)
