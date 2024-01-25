6ba03de0819ad3367eb964d160cb280b73a27053
wayne <waynetan0427@gmail.com>
2024-01-25 16:36:31 +0800

    modify num

--- genaipf/tools/search/utils/search_agent_utils.py
+++ genaipf/tools/search/utils/search_agent_utils.py
@@ -85,9 +85,10 @@ async def premise_search(newest_question, message_history, related_qa=None):
 def get_contents(contents):
     sources = []
     formatted_string = ''
-    for news_item in contents[:3]:
+    for index, news_item in enumerate(contents):
+        if index < 3:
+            formatted_string += f"{news_item.extract}\n引用地址: {news_item.url}\n"
         sources.append({"title": news_item.title, "url": news_item.url})
-        formatted_string += f"{news_item.extract}\n引用地址: {news_item.url}\n"
     return sources, formatted_string
 
 
