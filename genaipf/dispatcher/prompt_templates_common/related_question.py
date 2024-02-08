4403fc65281b57b24a09df86d48bc2a3ffbcbf20
wayne <waynetan0427@gmail.com>
2024-02-08 11:29:34 +0800

    modify related question lang

--- genaipf/dispatcher/prompt_templates_common/related_question.py
+++ genaipf/dispatcher/prompt_templates_common/related_question.py
@@ -1,5 +1,8 @@
 
-def _get_related_question_prompted_messages(data):
+def _get_related_question_prompted_messages(data, language):
+    prompt_language = '英文'
+    if language == 'zh':
+        prompt_language = '中文'
     messages = data["messages"]
 #     system_text = """
 # 你是一个善于沟通总结的专家，你可以根据用户的历史对话，给用户推荐 5 个用户感兴趣的相关问题，用 ';' 分开这 5 个问题。
@@ -12,9 +15,9 @@ def _get_related_question_prompted_messages(data):
 # 输出：
 # BTC最新价格预测是多少？;近期有哪些影响BTC的事件？;BTC的长期投资价值如何？;BTC交易平台哪个更安全？;BTC和ETH未来哪个更有前景？;
 # """
-    system_text = """
+    system_text = f"""
     你是一个善于沟通总结的专家，你可以根据用户的历史对话上下文生成五个类似问题，并以数组的形式返回。确保这些问题与原问题相关，但略有不同。
-    切记如果用户问题涉及违法操作，你只需回答 False
+    切记如果用户问题涉及违法操作，你只需回答 False，并且你回答的语言必须是{prompt_language}。
     例如：
     输入：
     user: 推荐买什么币？
