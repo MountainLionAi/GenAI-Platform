ea5c5b234b040d73a7e0f6509ef40130bfcef7cf
wayne <waynetan0427@gmail.com>
2024-04-25 17:39:01 +0800

    add more logs

--- genaipf/dispatcher/converter.py
+++ genaipf/dispatcher/converter.py
@@ -6,6 +6,7 @@ from genaipf.dispatcher.api import generate_unique_id, get_format_output, gpt_fu
 from genaipf.dispatcher.postprocess import posttext_mapping, PostTextParam
 from genaipf.tools.search.utils.search_agent_utils import not_need_search, not_need_sources
 from genaipf.services.cmc_token import get_token_cmc_url
+import time
 
 async def convert_func_out_to_stream(chunk, messages, newest_question, model, language, related_qa, source, owner, sources=[], is_need_search=False, sources_task=None, chain_id=''):
     """
@@ -23,7 +24,11 @@ async def convert_func_out_to_stream(chunk, messages, newest_question, model, la
     if sources:
         already_sources = True
     if is_need_search and (func_name not in not_need_sources):
+        convert_task_start_time = time.perf_counter()
         sources, related_qa = await sources_task
+        convert_task_emd_time = time.perf_counter()
+        elapsed_related_questions_task_time = (convert_task_start_time - convert_task_emd_time) * 1000
+        logger.info(f'=====================>convert_task_questions_task耗时：{elapsed_related_questions_task_time:.3f}毫秒')
     else:
         if func_name == 'coin_price':
             sources = [
