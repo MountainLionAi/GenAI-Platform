ef6b44ddc6916415ee721ec8093febc06b5f26a4
liu941125 <liu941125@gmail.com>
2024-05-07 10:39:57 +0800

    update

--- genaipf/dispatcher/converter.py
+++ genaipf/dispatcher/converter.py
@@ -89,7 +89,7 @@ async def convert_func_out_to_stream(chunk, messages, newest_question, model, la
     if (func_name not in not_need_search and sub_func_name != 'coin_swap1') or (func_name == 'generate_report' and presetContent == {}):
         _messages = [x for x in messages if x["role"] != "system"]
         msgs = _messages[::]
-        resp2 = await aref_answer_gpt_generator(msgs, model, language, _type, str(picked_content), related_qa, source, owner, output_type)
+        resp2 = await aref_answer_gpt_generator(msgs, model, language, _type, str(picked_content), related_qa, source, owner, output_type=output_type)
         logger.info(f'>>>>> start->data done.')
         async for item in resp2:
             if item["role"] == "inner_____gpt_whole_text":
