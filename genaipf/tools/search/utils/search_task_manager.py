b79c3ca439c162ed6131b5e6ec70c14063e57ccf
speak <speakjan9013@gmail.com>
2024-05-23 12:20:59 +0800

    modify rag

--- genaipf/tools/search/utils/search_task_manager.py
+++ genaipf/tools/search/utils/search_task_manager.py
@@ -217,9 +217,9 @@ async def multi_search(questions: str, related_qa=[], language=None):
     elif RAG_SEARCH_CLIENT == 'ALL':
         google_serper_client = GoogleSerperClient()
         multi_search_task.append(google_serper_client.search(questions))
-        multi_search_task.append(google_search(questions, 1, language, 'https://www.coindesk.com/'))
-        multi_search_task.append(google_search(questions, 1, language, 'https://www.reddit.com/'))
-        multi_search_task.append(google_search(questions, 1, language, 'https://www.chaincatcher.com/'))
+        multi_search_task.append(google_search(questions, 2, language, 'https://www.coindesk.com/'))
+        multi_search_task.append(google_search(questions, 2, language, 'https://www.reddit.com/'))
+        multi_search_task.append(google_search(questions, 2, language, 'https://www.chaincatcher.com/'))
         # multi_search_task.append(google_search(questions))
     #multi_search_task.append(metaphor_search2(questions, language))
     results = await asyncio.gather(*multi_search_task)
