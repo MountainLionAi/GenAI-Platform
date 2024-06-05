dcada161f33402f5d3df6aa1b4b7c1b03aaec9eb
liu941125 <liu941125@gmail.com>
2024-06-05 02:31:46 +0000

    update new type

--- genaipf/controller/gpt.py
+++ genaipf/controller/gpt.py
@@ -33,6 +33,13 @@ async def get_message_list(request: Request):
         message['create_time'] = message['create_time'].strftime('%Y-%m-%d %H:%M:%S')
         if message['type'] != 'user':
             message['content'] = json.loads(message['content'])
+        elif message['type'] != 'user_isDeep':
+            message['content'] = {
+                'type': 'user_isDeep',
+                'content': message['content'],
+            }
+            del message['base64content']
+            del message['quoteInfo']
         else:
             message['content'] = {
                 'type': 'user',
