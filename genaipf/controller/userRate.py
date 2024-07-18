f4387fc8077d54a06032469549bd2dcee43f2c38
speak <speakjan9013@gmail.com>
2024-07-18 01:30:00 +0000

    modify share summary

--- genaipf/controller/userRate.py
+++ genaipf/controller/userRate.py
@@ -44,9 +44,13 @@ async def share_message(request: Request):
             if m["role"] in ["system", "user", "assistant"]:
                 if m.get("type", "") == "preset4":
                     summary_str = f'{m["content"]["coinName"]}币价预测'
+                    if language == 'en':
+                        summary_str = f'{m["content"]["coinName"]} Daily Coin price predict'
                     break
                 elif m.get("type", "") == "preset7":
                     summary_str = f'{m["content"]["predict"]["coinName"]}研报'
+                    if language == 'en':
+                        summary_str = f'{m["content"]["predict"]["coinName"]} research report'
                     break
         if summary_str == '':
             summary_str = await get_share_summary(messages, language)
