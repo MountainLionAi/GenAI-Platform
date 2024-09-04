e46420ea6211a7a9c4c435ac5455111823076c17
wayne <waynetan0427@gmail.com>
2024-09-04 18:12:50 +0800

    modify

--- genaipf/services/user_service.py
+++ genaipf/services/user_service.py
@@ -187,7 +187,7 @@ async def user_register(email, password, verify_code, inviter):
         if user_infos and len(user_infos) != 0:
             user_info = user_infos[0]
             from ml4gp.service.points_service import create_user_points_account
-            await create_user_points_account[user_info['id']]
+            await create_user_points_account(user_info['id'])
         if inviter:
             from ml4gp.services.points_activity_service import create_user_points_act, POINTS_ACTIVITY_INVITATION
             await create_user_points_act({}, POINTS_ACTIVITY_INVITATION, inviter)
