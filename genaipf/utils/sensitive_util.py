a3a40205d8cd2e28c68a3a7f4748dc215c6e51da
speak <speakjan9013@gmail.com>
2024-09-23 09:12:23 +0000

    modify sensitive words

--- genaipf/utils/sensitive_util.py
+++ genaipf/utils/sensitive_util.py
@@ -1,9 +1,11 @@
 from genaipf.constant.sensitive_words import WORDS
+from genaipf.utils.log_utils import logger
 
 async def isNormal(str):
     isNormal = True
     for word in WORDS:
         if word in str:
+            logger.info('sensitive_words:'+word)
             isNormal = False
             break
     return isNormal
