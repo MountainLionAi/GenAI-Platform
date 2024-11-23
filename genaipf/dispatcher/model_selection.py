a7004e77c725ab30553d1f60d5a9376ce6429612
liu941125 <liu941125@gmail.com>
2024-11-23 02:26:38 +0000

    update

--- genaipf/dispatcher/model_selection.py
+++ genaipf/dispatcher/model_selection.py
@@ -3,11 +3,15 @@ from genaipf.dispatcher.utils import async_simple_chat
 from genaipf.conf.model_selection_conf import MODEL_MAPPING
 from genaipf.utils.log_utils import logger
 async def check_and_pick_model(newest_question, model):
-    prompt = _get_question_type_prompted_messages(newest_question)
-    msg_type = await async_simple_chat(prompt)
-    logger.info(f"gpt判断类别{msg_type}")
-    if msg_type in MODEL_MAPPING:
-        model = MODEL_MAPPING[msg_type]
-    else:
-        model = 'openai'
-    return model 
+    try:
+        prompt = _get_question_type_prompted_messages(newest_question)
+        msg_type = await async_simple_chat(prompt)
+        logger.info(f"gpt判断类别{msg_type}")
+        if msg_type in MODEL_MAPPING:
+            model = MODEL_MAPPING[msg_type]
+        else:
+            model = 'openai'
+        return model 
+    except Exception as e:
+        logger.info("model selection error, use openai as default")
+        return 'openai'
