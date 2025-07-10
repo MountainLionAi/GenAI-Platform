db6ade33d095ce85dd64fd8a4cacebe3da212528
wayne <waynetan0427@gmail.com>
2025-07-10 16:40:50 +0800

    remove times check

--- genaipf/controller/gptstream.py
+++ genaipf/controller/gptstream.py
@@ -139,24 +139,25 @@ userid={userid},language={language},msggroup={msggroup},device_no={device_no},qu
     # messages = [{"role": msg["role"], "content": msg["content"]} for msg in process_messages(messages)]
     # messages = messages[-10:]
     messages = process_messages(messages)
-    try:
-        # v201、v202 swft移动端，v203 mlion tgbot，v204 external对外开放，v210 swftGpt
-        source_list = ['v005', 'v006', 'v008', 'v009', 'v010', 'v201', 'v202', 'v203', 'v204', 'v210', 'v013']
-        if (
-                not IS_UNLIMIT_USAGE and not IS_INNER_DEBUG) and model == 'ml-plus' and source not in source_list and without_minus == 0 and not any(
-                item.get("type") in ["ai_auto_recommand", "ai_plugin_swap"]  for item in messages):
-            _user_id = ''
-            if userid != 0:
-                _user_id = userid
-            can_use = await points_service.check_user_can_use_time(_user_id, visitor_id)
-            # can_use = await user_account_service_wrapper.get_user_can_use_time(userid)
-            if can_use:
-                await points_service.minus_user_can_use_time(_user_id, 'query', visitor_id)
-            else:
-                return fail(ERROR_CODE['NO_REMAINING_TIMES'])
-    except Exception as e:
-        logger.error(e)
-        logger.error(traceback.format_exc())
+    # TODO 暂时取消聊天的收费
+    # try:
+    #     # v201、v202 swft移动端，v203 mlion tgbot，v204 external对外开放，v210 swftGpt
+    #     source_list = ['v005', 'v006', 'v008', 'v009', 'v010', 'v201', 'v202', 'v203', 'v204', 'v210', 'v013']
+    #     if (
+    #             not IS_UNLIMIT_USAGE and not IS_INNER_DEBUG) and model == 'ml-plus' and source not in source_list and without_minus == 0 and not any(
+    #             item.get("type") in ["ai_auto_recommand", "ai_plugin_swap"]  for item in messages):
+    #         _user_id = ''
+    #         if userid != 0:
+    #             _user_id = userid
+    #         can_use = await points_service.check_user_can_use_time(_user_id, visitor_id)
+    #         # can_use = await user_account_service_wrapper.get_user_can_use_time(userid)
+    #         if can_use:
+    #             await points_service.minus_user_can_use_time(_user_id, 'query', visitor_id)
+    #         else:
+    #             return fail(ERROR_CODE['NO_REMAINING_TIMES'])
+    # except Exception as e:
+    #     logger.error(e)
+    #     logger.error(traceback.format_exc())
 
     try:
         async def event_generator(_response):
