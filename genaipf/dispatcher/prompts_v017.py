6b1092cb421caf5ea59f96d0540a28475f09ceb5
wayne <waynetan0427@gmail.com>
2026-02-13 17:55:36 +0800

    add v017

---
+++ genaipf/dispatcher/prompts_v017.py
@@ -0,0 +1,26 @@
+from importlib import import_module
+from genaipf.conf.server import PLUGIN_NAME
+
+from genaipf.dispatcher.prompt_templates_v001.default import _get_default_afunc_prompt, _get_default_aref_answer_prompt, _get_default_merge_ref_and_input_text
+
+_default_lang = "en"
+class LionPrompt:
+    default_lang = _default_lang
+    
+    @classmethod
+    def get_afunc_prompt(cls, language=_default_lang, picked_content="", related_qa=[], model='', data={}):
+        return _get_default_afunc_prompt(language, picked_content, related_qa, model, data)
+
+    @classmethod
+    def get_aref_answer_prompt(cls, language=_default_lang, preset_name=None, picked_content="", related_qa=[], model='', data={}):
+        return _get_default_aref_answer_prompt(language, picked_content, related_qa, model, data)
+
+    @classmethod
+    def get_merge_ref_and_input_prompt(cls, ref, related_qa, input_text, language=_default_lang, preset_name=None, data={}):
+        return _get_default_merge_ref_and_input_text(ref, related_qa, input_text, language)
+            
+if PLUGIN_NAME:
+    plugin_submodule_name = f'{PLUGIN_NAME}.dispatcher.prompts_v017'
+    plugin_submodule = import_module(plugin_submodule_name)
+    LionPrompt = plugin_submodule.LionPrompt
+
